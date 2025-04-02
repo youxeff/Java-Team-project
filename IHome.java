9045f86a1a1d6c7868e178a34e91dd449bcceb4f
murph622 <murph622@purdue.edu>
2025-04-02 18:08:26 -0400

    Add files via upload
    interfaces for categorized item classes

---
+++ IHome.java
@@ -0,0 +1,10 @@
+/**
+ * This interface represents a home item.
+ * The fields of the Home class that will extend IHome will be the same as listed in the Item interface, but
+ * will also include fields like:
+ *  type: String, type of item ("Furniture", "Decoration", etc.)
+ */
+public interface IHome extends Item{
+    // inherits all methods from Item
+    // will include getters and setters for additional fields as listed above
+}
