3404c89953d71047777fde4915a34cd769bababd
Youssef Abdelkader <youssefabdelkader7@gmail.com>
2025-04-04 16:39:10 -0400

    Remove Main.java and add Testing.java for user registration and login functionality

--- Testing.java
+++ Testing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Testing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Seller seller = null;
