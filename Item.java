368228cc17ee8ea7d7586dee52d5b378d2812f56
murph622 <murph622@purdue.edu>
2025-04-04 21:14:15 -0400

    fixing item classes

--- Item.java
+++ Item.java
@@ -1,55 +1,23 @@
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// /**
-//  * This interface represents an item. It will be implemented in classes that will represent items of a specific category.
-//  * These classes will have the following fields:
-//  * name: String, name of the item
-//  * cost: double, cost of the item
-//  * soldBy: Seller, seller selling the item
-//  * isAvailable: boolean, whether the item is available to buy (true if available)
-//  * image: String, path to image of the item
-//  */
-// interface Item {
-//     /**
-//      * Processes the sale of the item to a buyer. Returns true if successful and false if not.
-//      *
-//      * @param buyer, The buyer of the item.
-//      * @return Boolean, true if sale goes through
-//      */
-//     Boolean sellItem(Buyer buyer);
+public interface Item {
+    // Transaction methods
+    Boolean sellItem(Buyer buyer);
+    Boolean deleteItem();
+    void markSold();
 
-//     /**
-//      * Removes the item listing. Returns true if successful and false if not.
-//      *
-//      * @return Boolean, true if deletion goes through
-//      */
-//     Boolean deleteItem();
+    // Search methods (these might be better in Marketplace)
+    ArrayList<Item> searchByName(String name);
+    ArrayList<Item> searchByCategory(String category);
 
-//     /**
-//      * Search items by name
-//      *
-//      * @param name, The name of the item.
-//      * @return List<Item>, list of matches
-//      */
-//     ArrayList<Item> searchByName(String name);
+    // Display method
+    String toString();
 
-//     /**
-//      * Search items by category
-//      *
-//      * @param category, The category of the item.
-//      * @return List<Item>, list of matches
-//      */
-//     ArrayList<Item> searchByCategory(String category);
-
-//     /**
-//      * Mark item as sold (change isAvailable to false)
-//      */
-//     void markSold();
-
-//     /**
-//      * Returns a formatted string with all the details (name, cost, seller, etc.). Overrides Object superclass’s toString method.
-//      *
-//      * @return String, string representation of the item
-//      */
-//     String toString();
-// }
+    // Getter methods
+    String getName();
+    double getCost();
+    Seller getSoldBy();
+    boolean isAvailable();
+    String getImage();
+    String getCategory();
+}
