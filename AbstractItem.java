6e1bd356a2ffdb4262c6f986c9b11bfcd1365a39
murph622 <murph622@purdue.edu>
2025-04-04 21:15:37 -0400

    created abstract item

---
+++ AbstractItem.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public abstract class AbstractItem implements Item {
+    protected String name;
+    protected double cost;
+    protected Seller soldBy;
+    protected boolean isAvailable;
+    protected String image;
+    protected String category;
+
+    public AbstractItem(String name, double cost, Seller soldBy, String image, String category) {
+        this.name = name;
+        this.cost = cost;
+        this.soldBy = soldBy;
+        this.isAvailable = true;
+        this.image = image;
+        this.category = category;
+    }
+
+    @Override
+    public Boolean sellItem(Buyer buyer) {
+        if (!isAvailable || buyer.getBalance() < cost) {
+            return false;
+        }
+        buyer.setBalance(buyer.getBalance() - cost);
+        soldBy.setBalance(soldBy.getBalance() + cost);
+        markSold();
+        return true;
+    }
+
+    @Override
+    public Boolean deleteItem() {
+        isAvailable = false;
+        return true;
+    }
+
+    @Override
+    public ArrayList<Item> searchByName(String name) {
+        // This would be implemented in the marketplace class
+        return new ArrayList<>();
+    }
+
+    @Override
+    public ArrayList<Item> searchByCategory(String category) {
+        // This would be implemented in the marketplace class
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void markSold() {
+        isAvailable = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - $%.2f - Sold by: %s - %s",
+                name, cost, soldBy.getUserName(), isAvailable ? "Available" : "Sold");
+    }
+
+    // Common getters and setters
+    public String getName() { return name; }
+    public double getCost() { return cost; }
+    public Seller getSoldBy() { return soldBy; }
+    public boolean isAvailable() { return isAvailable; }
+    public String getImage() { return image; }
+    public String getCategory() { return category; }
+}
