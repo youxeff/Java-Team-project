c867e0c135515c9abbaa4a8211d7f46694238c4c
murph622 <murph622@purdue.edu>
2025-04-04 21:17:04 -0400

    fixing item classes

---
+++ Home.java
@@ -0,0 +1,19 @@
+public class Home extends AbstractItem {
+    private String type;
+
+    public Home(String name, double cost, Seller soldBy, String image,
+                String category, String type) {
+        super(name, cost, soldBy, image, category);
+        this.type = type;
+    }
+
+    // home specific methods
+    public String getType() { return type; }
+
+    public void setType(String type) { this.type = type; }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" - Type: %s", type);
+    }
+}
