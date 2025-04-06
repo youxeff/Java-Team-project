7ffbb6926d3df147ec9ee14a5831306807f05e59
murph622 <murph622@purdue.edu>
2025-04-06 12:14:36 -0400

    added javadocs

--- model/items/Vehicle.java
+++ model/items/Vehicle.java
@@ -1,12 +1,27 @@
 package model.items;
 import model.users.User;
 
-public class Vehicle extends AbstractItem {
+/**
+ * Represents a vehicle item in the marketplace.
+ * Extends AbstractItem and implements IVehicle interface.
+ */
+public class Vehicle extends AbstractItem implements IVehicle {
     private int mileage;
     private int year;
     private String brand;
     private final Object lock = new Object();
 
+    /**
+     * Constructs a new Vehicle item
+     * @param name The name of the vehicle
+     * @param cost The price of the vehicle
+     * @param soldBy The user selling the vehicle
+     * @param image Image path/URL for the vehicle
+     * @param category Category of the vehicle
+     * @param mileage Mileage of the vehicle
+     * @param year Manufacturing year of the vehicle
+     * @param brand Brand of the vehicle
+     */
     public Vehicle(String name, double cost, User soldBy, String image,
                    String category, int mileage, int year, String brand) {
         super(name, cost, soldBy, image, category);
@@ -15,13 +30,18 @@ public class Vehicle extends AbstractItem {
         this.brand = brand;
     }
 
-    // IVehicle specific methods
+    @Override
     public synchronized int getMileage() { return mileage; }
+    @Override
     public synchronized int getYear() { return year; }
+    @Override
     public synchronized String getBrand() { return brand; }
 
+    @Override
     public synchronized void setMileage(int mileage) { this.mileage = mileage; }
+    @Override
     public synchronized void setYear(int year) { this.year = year; }
+    @Override
     public synchronized void setBrand(String brand) { this.brand = brand; }
 
     @Override
