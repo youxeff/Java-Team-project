d484a0c45dc8914460a8fe3fd1d41036190cff55
murph622 <murph622@purdue.edu>
2025-04-06 12:15:22 -0400

    added javadocs

--- model/items/Item.java
+++ model/items/Item.java
@@ -1,20 +1,47 @@
 package model.items;
 import java.util.ArrayList;
-
 import model.users.User;
 
+/**
+ * Interface defining the common functionality for all items in the marketplace.
+ */
 public interface Item {
-    // Transaction methods
+    /**
+     * Attempts to sell the item to a user
+     * @param buyer The user attempting to buy the item
+     * @return true if the sale was successful, false otherwise
+     */
     Boolean sellItem(User buyer);
 
+    /**
+     * Marks the item as deleted (unavailable)
+     * @return true if the operation was successful
+     */
     Boolean deleteItem();
+    
+    /**
+     * Marks the item as sold
+     */
     void markSold();
 
-    // Search methods (these might be better in Marketplace)
+    /**
+     * Searches for items by name (to be implemented in Marketplace)
+     * @param name The name to search for
+     * @return List of matching items
+     */
     ArrayList<Item> searchByName(String name);
+    
+    /**
+     * Searches for items by category (to be implemented in Marketplace)
+     * @param category The category to search for
+     * @return List of matching items
+     */
     ArrayList<Item> searchByCategory(String category);
 
-    // Display method
+    /**
+     * Returns a string representation of the item
+     * @return String representation
+     */
     String toString();
 
     // Getter methods
