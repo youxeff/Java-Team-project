e5e34e10223dc8006b9e5543eaea44f63348b4ec
Youssef Abdelkader <youssefabdelkader7@gmail.com>
2025-04-06 23:14:33 -0400

    Added IMain.java and

---
+++ Main/IMain.java
@@ -0,0 +1,13 @@
+package Main;
+
+public interface IMain {
+
+    void registerUser();
+    void loginUser();
+    void showUserMenu();
+    void displayUserProfile();
+    void updateBalance();
+    void buyOrSell();
+    void sellItem();
+    void buyItem();
+}
