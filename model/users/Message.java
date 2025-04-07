52a733353e30faae60f6d510cdb8f0a185b4b629
eyen0215 <138848557+eyen0215@users.noreply.github.com>
2025-04-06 23:13:25 -0400

    Add javadoc for message class

--- model/users/Message.java
+++ model/users/Message.java
@@ -1,4 +1,10 @@
 package model.users;
+
+/**
+ * This program creates an Interface for the MarketplaceUser class.
+ * This is implemented in the MarketplaceUser class for direct messaging purposes.
+ */
+
 public interface Message {
     
     void sendMessageTo(String recipientName, String message);
