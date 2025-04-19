99ff20e9d872c1065c117124573e2d1673efd753
murph622 <murph622@purdue.edu>
2025-04-19 18:33:05 -0400

    added javadocs to IElectronic.java

--- model/items/IElectronic.java
+++ model/items/IElectronic.java
@@ -1,12 +1,29 @@
 package model.items;
 
 /**
- * Interface for electronic items
+ * Defines the interface for electronic items in the marketplace.
+ * Specifies electronic-specific properties and operations.
  */
-
 public interface IElectronic {
+    /**
+     * @return The type of electronic item
+     */
     String getType();
+    
+    /**
+     * @return The manufacturing year of the electronic item
+     */
     int getYear();
+    
+    /**
+     * Sets the type of electronic item.
+     * @param type New type value
+     */
     void setType(String type);
+    
+    /**
+     * Sets the manufacturing year of the electronic item.
+     * @param year New year value
+     */
     void setYear(int year);
 }
