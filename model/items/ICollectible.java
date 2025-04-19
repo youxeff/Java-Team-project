2327f44e585b50f9ba246bc7f76d67f6a8fa48b2
murph622 <murph622@purdue.edu>
2025-04-19 18:33:29 -0400

    added javadocs to ICollectible.java

--- model/items/ICollectible.java
+++ model/items/ICollectible.java
@@ -1,11 +1,29 @@
 package model.items;
 
 /**
- * Interface for collectible items
+ * Defines the interface for collectible items in the marketplace.
+ * Specifies collectible-specific properties and operations.
  */
 public interface ICollectible {
+    /**
+     * @return The type of collectible
+     */
     String getType();
+    
+    /**
+     * @return The condition of the collectible
+     */
     String getCondition();
+    
+    /**
+     * Sets the type of collectible.
+     * @param type New type value
+     */
     void setType(String type);
+    
+    /**
+     * Sets the condition of the collectible.
+     * @param condition New condition value
+     */
     void setCondition(String condition);
 }
